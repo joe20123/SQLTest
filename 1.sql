d369c6cd5c237b423840b0a98ec1ad631ced43a4
DEXION.AU\joseph.wu <wuzhou88@gmail.com>
2018-07-24 13:03:17 +1000

    create tables for isolation test

--- 1.sql
+++ 1.sql
@@ -1,3 +1,4 @@
+--create a new table
 CREATE SCHEMA Arts;
 GO
 CREATE TABLE Arts.Performer
@@ -31,6 +32,41 @@ GO
 -- where account = '1111'
 
 -- COMMIT TRANSACTION
+
+
+-- isolation test.
+-- isolation levels: Read Uncommitted, Read Committed, 
+--                   Repeatable Read, Serializable
+CREATE SCHEMA Art;
+GO
+Create TABLE Art.Artist
+(
+    ArtistID int CONSTRAINT PKArtist PRIMARY KEY,
+    Name VARCHAR(30),
+    Padding char(4000) DEFAULT (replicate('a', 4000))
+);
+INSERT Into Art.Artist(ArtistID, Name)
+VALUES (1, 'da Vinci'), 
+       (2, 'Micheangelo'), 
+       (3, 'Donatello'),
+       (4, 'Picasso'), 
+       (5, 'Dali'), 
+       (6, 'Jones');
+GO
+CREATE TABLE Art.ArtWork
+(
+    ArtWorkId int CONSTRAINT PKArtWork PRIMARY KEY,
+    ArtistId int NOT NULL CONSTRAINT FKArtwork$wasDoneBy$Artist REFERENCES Art.Artist (ArtistID),
+    Name VARCHAR(30),
+    Padding char(4000) DEFAULT (replicate('a', 4000)),
+    CONSTRAINT AKArtwork UNIQUE (ArtistId, Name)
+);
+Insert into Art.ArtWork (ArtWorkId, ArtistId, Name)
+VALUES (1, 1, 'last Supper'),
+       (2, 1, 'Mona Lisa'),
+       (3, 6, 'Rabbit Fire');
+GO
+
 
 
 
